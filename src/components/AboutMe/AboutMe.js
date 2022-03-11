6101b265529100b6eba0104f691607c9695369c1
Alexandr Ivanov <Kurotsuchi346@yandex.ru>
2022-03-12 01:46:45 +0300

    add adaptive footer, portfolio, aboutme, techs

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -6,14 +6,15 @@ export default function AboutMe(props) {
 	return (
 		<section className="about-me">
 			<h2 className="about-me__title">Студент</h2>
-			<div className="about-me__wrapper"></div>
-			<img src={imageUser} className="about-me__photo" alt="мое фото" />
-			<div className="about-me__info">
-				<h3 className="about-me__subtitle">Александр</h3>
-				<p className="about-me__profession">Фронтенд-разработчик, 26 лет</p>
-				<p className="about-me__description">
-					Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.
-				</p>
+			<div className="about-me__wrapper">
+				<img src={imageUser} className="about-me__photo" alt="мое фото" />
+				<div className="about-me__info">
+					<h3 className="about-me__subtitle">Александр</h3>
+					<p className="about-me__profession">Фронтенд-разработчик, 26 лет</p>
+					<p className="about-me__description">
+						Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.
+					</p>
+				</div>
 			</div>
 			<div className="about-me__links" >
 				<a className="about-me__link" href="https://ru-ru.facebook.com/">Facebook</a>
--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -5,20 +5,22 @@ export default function AboutProject(props) {
 	return (
 		<section className="about-project">
 			<h2 className="about-project__title">О проекте</h2>
-			<div className="about-project__info">
-				<h3 className="about-project__subtitle">Дипломный проект включал 5 этапов</h3>
-				<p className="about-project__info-description">
-					Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.
-				</p>
-			</div>
-			<div className="about-project__info">
-				<h3 className="about-project__subtitle">На выполнение диплома ушло 5 недель</h3>
-				<p className="about-project__info-description">
-					У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.
-				</p>
+			<div className="about-project__info-wrapper">
+				<div className="about-project__info">
+					<h3 className="about-project__subtitle">Дипломный проект включал 5 этапов</h3>
+					<p className="about-project__info-description">
+						Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.
+					</p>
+				</div>
+				<div className="about-project__info">
+					<h3 className="about-project__subtitle">На выполнение диплома ушло 5 недель</h3>
+					<p className="about-project__info-description">
+						У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.
+					</p>
+				</div>
 			</div>
 			<div className="about-project__time">
-				<div className="about-project__time-item">
+				<div className="about-project__time-item about-project__time-item_position_left">
 					<p className="about-project__time-text about-project__time-text_color_black">1 неделя</p>
 					<p className="about-project__time-text about-project__time-text_color_gray">Back-end</p>
 				</div>
