51ff6a73a87f1be5a025c7d9aa72ddc6a962ed1d
Alexandr Ivanov <Kurotsuchi346@yandex.ru>
2022-03-19 19:52:42 +0300

    fix bugs after review

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -6,8 +6,8 @@ export default function SearchForm(props) {
     <section className="search-form">
       <form className="search-form__form">
         <div className="search-form__form-wrapper">
-          <input className="search-form__input-text" type="text" placeholder="Фильм" name="search" />
-          <button className="search-form__form-button">Найти</button>
+          <input className="search-form__input-text" required type="text" placeholder="Фильм" name="search" />
+          <button className="search-form__form-button" type="submit">Найти</button>
         </div>
         <div className="search-form__filter">
           <input className="search-form__custom-checkbox" type="checkbox" id="Короткометражки" name="filter" />
